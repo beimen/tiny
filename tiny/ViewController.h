61b58bcf4f4e6d1fa0073283a3e950d73e2a4496
beimenjun <beimenjun@gmail.com>
2015-12-07 23:11:46 +0800

    Initial Commit

---
+++ tiny/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tiny
+//
+//  Created by Xie Hong on 12/7/15.
+//  Copyright © 2015 timecoco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tiny/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tiny
+//
+//  Created by Xie Hong on 12/7/15.
+//  Copyright © 2015 timecoco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
